2b5654264eca293612fab7d484714d326924f072
brandon <brandonmassie32@gmail.com>
2024-07-12 14:31:17 +0000

    changes to nginx confs

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ ln -s /usr/lib/x86_64-linux-gnu/libbson-1.0.so /usr/lib/x86_64-linux-gnu/bson-1.
 ln -s /usr/lib/x86_64-linux-gnu/libbson-1.0.so /usr/local/lib/bson-1.0
 ln -s /usr/lib/x86_64-linux-gnu/libbson-1.0.so /usr/lib/bson-1.0
 
-
-
 luarocks install lua-mongo 
 
-openresty -g "daemon off;" -c /etc/nginx/nginx.conf
+openresty -c /etc/nginx/nginx.conf 
